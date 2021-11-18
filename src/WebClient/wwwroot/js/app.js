21af837910dbea948e935d2848e000c0ce7fa08e
Giuseppe R <rayman85gr@gmail.com>
2021-11-18 14:57:35 +0000

    fix: fixed mobile theme icon not showing

--- src/WebClient/wwwroot/js/app.js
+++ src/WebClient/wwwroot/js/app.js
@@ -4,32 +4,41 @@ function readLocalStorage(key) { return localStorage[key]; }
 function checkThemeSwitch() {
     
     const checkbox = document.querySelector("input[id=theme-switch]");
+    const themeIcon = document.querySelector("span[id=theme-icon]");
 
     if (checkbox == null) return;
+
+    console.log("isDarkModeOn: ", isDarkModeOn);
+
+    if (isDarkModeOn === false) {
+        themeIcon.className = "oi oi-moon theme-icon";
+    }
+    else {
+        themeIcon.className = "oi oi-sun theme-icon";
+    }
 
     checkbox.addEventListener('change', function () {
 
         if (document.body.className == "light-mode") {
-            document.body.className ='dark-mode';
+            document.body.className = 'dark-mode';
+            themeIcon.className = "oi oi-moon theme-icon";
             addToLocalStorage("theme", true);
         } else {
             document.body.className = 'light-mode';
+            themeIcon.className = "oi oi-sun theme-icon";
             addToLocalStorage("theme", false);
         }
     });
+
+    console.log(themeIcon);
 }
 
-var isDarkModeOn = readLocalStorage("theme");
+const isDarkModeOn = readLocalStorage("theme");
 
-if (isDarkModeOn == null) {
+if (isDarkModeOn === "true") {
     document.body.classList.toggle('dark-mode');
 }
 else {
-    if (isDarkModeOn === "true") {
-        document.body.classList.toggle('dark-mode');
-    }
-    else {
-        document.body.classList.toggle('light-mode');
-    }
-}
+    document.body.classList.toggle('light-mode');
+};
 
