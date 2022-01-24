7aaa91e66c85afc3f8ad529ce833045e8ca82f5b
joe85gr <joe85gr>
2022-01-24 21:18:58 +0000

    feat: implemented polly retry policy

--- src/Server/Extensions/HttpClientExtensions.cs
+++ src/Server/Extensions/HttpClientExtensions.cs
@@ -3,11 +3,15 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Polly;
 
 namespace Server.Extensions;
 
 public static class HttpClientExtensions
 {
+    private const int MaxRetries = 3;
+    private const int RetryDelay = 500;
+
     public static async Task<T> GetContentAsync<T>(this HttpClient client, HttpRequestMessage request, 
         CancellationToken cancellationToken = default)
     {
@@ -16,8 +20,12 @@ public static class HttpClientExtensions
         var response = await client.SendAsync(request, cancellationToken);
         
         response.EnsureSuccessStatusCode();
+
+        var retryPolicy = Policy.Handle<Exception>()
+            .WaitAndRetry(MaxRetries, attemptCount => TimeSpan.FromMilliseconds(attemptCount * RetryDelay));
+
+        var responseStream = await retryPolicy.Execute(() => response.Content.ReadAsStreamAsync(cancellationToken));
         
-        var responseStream = await response.Content.ReadAsStreamAsync(cancellationToken);
         var content = await JsonSerializer.DeserializeAsync<T>(responseStream, cancellationToken: cancellationToken);
 
         return content;
